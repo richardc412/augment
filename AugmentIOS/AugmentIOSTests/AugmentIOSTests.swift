b38c5a475168ea6803cd759f1eac0be1c0cc4da2
Richard <idk.duderc@gmail.com>
2025-07-23 21:36:45 -0700

    Initialized AR Project Again

---
+++ AugmentIOS/AugmentIOSTests/AugmentIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  AugmentIOSTests.swift
+//  AugmentIOSTests
+//
+//  Created by Richard Cao on 2025-07-23.
+//
+
+import Testing
+@testable import AugmentIOS
+
+struct AugmentIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
